b9b0fff6c8a8ba42f342c49c7cc36169a3089feb
aasehic <aasehic@miners.utep.edu>
2020-03-06 16:55:56 -0700

    slighty less humble beginnings

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,10 +1,66 @@
 public class SortOfSort {
 
-    public static void sortOfSort(int[] A) {
-
+    public void sortOfSort(int[] A) {
+        if (A.length < 3) {
+            miniSort(A);
+        } else {
+            sortHelper(A, 0, 0, 0, 0, 0, 0);
+        }
     }
 
-    public int[] sortHelper (int[] A, int start, int offset, int counter, int counter2) {
+    public int[] sortHelper (int[] A, int start, int offset, int counter, int counter2, int base, int base2) {
+        int max = A[start];
+        int last_index = (A.length - 1) - offset;
+        int mid = A.length / 2;
+        int temp = A[last_index];
 
+        if ((base >= mid) && (base2 >= mid)) {
+            return A;
+        }
+        if ((counter == 2) && (counter2 == 2)) {
+            counter = 0;
+            counter2 = 0;
+        }
+        for (int i = start; i <= last_index; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                if (counter < 2) {
+                    temp = A[last_index];
+                } else {
+                    temp = A[start];
+                }
+            }
+        }
+
+        for (int i = start; i <= last_index; i++) {
+            if (A[i] == max) {
+                A[i] = temp;
+                break;
+            }
+        }
+        if (counter < 2) {
+            A[last_index] = max;
+            sortHelper(A,start,offset + 1,counter + 1, counter2, base + 1, base2);
+        }  else {
+            A[start] = max;
+            sortHelper(A,start + 1, offset, counter, counter2 + 1, base, base2 + 1);
+        }
+        return A;
+    }
+
+    public int[] miniSort(int[] A) {
+        int temp = 0;
+        if (A.length == 1) {
+            return A;
+        } else {
+            if (A[0] > A[1]) {
+                temp = A[1];
+                A[1] = A[0];
+                A[0] = temp;
+                return A;
+            } else {
+                return A;
+            }
+        }
     }
 }
---
+++ java/src/SortOfSortTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortOfSortTest {
+    // Generic Test to ensure that the code runs properly
+    @Test
+    public void sortOfSort() {
+        int[] A = {1,4,3,17,44,6};
+        int[] B = {6,4,1,3,17,44};
+        SortOfSort test = new SortOfSort();
+        test.sortOfSort(A);
+        assertArrayEquals(A,B);
+    }
+    // It passed
+
+    // Generic Test to see if my code can handle odd length areas properly
+    @Test
+    public void sortOfSort2() {
+        int[] A = {2,8,33,91,4,-1,5};
+        int[] B = {8,5,-1,2,4,33,91};
+        SortOfSort test = new SortOfSort();
+        test.sortOfSort(A);
+        assertArrayEquals(A,B);
+    }
+    // It passed
+
+    // Fairly Generic Test, but due to how i set up my base case, this is to test if there is any issues when the array is smaller then 4 and even
+    @Test
+    public void sortOfSort3() {
+        int[] A = {8,4};
+        int[] B = {4,8};
+        SortOfSort test = new SortOfSort();
+        test.sortOfSort(A);
+        assertArrayEquals(A,B);
+    }
+    // It passed
+
+    //Same as the previous test, but smaller then 4 and odd
+    @Test
+    public void sortOfSort4() {
+        int[] A = {1,9,8};
+        int[] B = {1,8,9};
+        SortOfSort test = new SortOfSort();
+        test.sortOfSort(A);
+        assertArrayEquals(A,B);
+    }
+    // It passed
+
+    //Simple Test to see if the code bugs out or breaks when encountering duplicates
+    @Test
+    public void sortOfSort5() {
+        int[] A = {13,4,23,23,8,12};
+        int[] B = {13,12,4,8,23,23};
+        SortOfSort test = new SortOfSort();
+        test.sortOfSort(A);
+        assertArrayEquals(A,B);
+    }
+    // It passed
+}
